500b73c85083100261fd24a57ee87ac4d20c1b54
brycegoh <brycegoh96@gmail.com>
2020-06-20 22:52:39 +0800

    prettied and added cors handler

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const passport = require('passport');
+const passport = require("passport");
 const cors = require("cors");
-require('dotenv').config();
-const {verifyToken } = require('./config/auth')
+require("dotenv").config();
+const { verifyToken, corsHandler } = require("./config/auth");
 
 //config
 const port = process.env.PORT || 5000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
 //mongodb
-mongoose.connect( MONGODB_URL, 
-    { 
-        useNewUrlParser: true, 
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    }
-)
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const dbConnection = mongoose.connection;
-dbConnection.once('open',()=>{
-    console.log("Connected to db")
-})
+dbConnection.once("open", () => {
+  console.log("Connected to db");
+});
 
 //middleware
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-
-require('./config/passport')(passport)
-app.use(passport.initialize())
-app.use(cors());
+app.use(corsHandler);
+require("./config/passport")(passport);
+app.use(passport.initialize());
+// app.use(cors({ origin: true, credentials: true }));
 
 app.use(verifyToken);
-app.use(require('./routes'))
+app.use(require("./routes"));
 
 //port
-app.listen(port, ()=>{
-    console.log("SERVER RUNNING LO")
-})
-
-
+app.listen(port, () => {
+  console.log("SERVER RUNNING LO");
+});
 
 // error handling middleware
-function errorHandler (err , req, res, next){
-    console.log(err)
+function errorHandler(err, req, res, next) {
+  console.log(err);
 }
 
 app.use(errorHandler);
-
